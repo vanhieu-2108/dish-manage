8e2043c0f1c751c3dd4bfb9a1b31ca11654c10e7
hieule2108 <levanhieusona@gmail.com>
2024-06-20 16:37:45 +0700

    Setup Tanstack Query và code logic login

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,7 @@
+import { toast } from "@/components/ui/use-toast";
+import { EntityError } from "@/lib/http";
 import { type ClassValue, clsx } from "clsx";
+import { UseFormSetError } from "react-hook-form";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -7,4 +10,30 @@ export function cn(...inputs: ClassValue[]) {
 
 export const normalizePath = (path: string) => {
   return path.startsWith("/") ? path.slice(1) : path;
+};
+
+export const handleErrorApi = ({
+  error,
+  setError,
+  duration,
+}: {
+  error: any;
+  setError?: UseFormSetError<any>;
+  duration?: number;
+}) => {
+  if (error instanceof EntityError && setError) {
+    error.payload.errors.forEach((item) => {
+      setError(item.field, {
+        type: "server",
+        message: item.message,
+      });
+    });
+  } else {
+    toast({
+      title: "Lỗi",
+      description: error?.payload?.message ?? "Lỗi không xác định",
+      variant: "destructive",
+      duration: duration ?? 5000,
+    });
+  }
 };
