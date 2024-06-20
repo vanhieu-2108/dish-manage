96d127c232a5e97e602e6fe67de7cce2cb73482e
hieule2108 <levanhieusona@gmail.com>
2024-06-20 14:29:58 +0700

    Tạo file config để validate env

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-
+import envConfig from "../../config";
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
